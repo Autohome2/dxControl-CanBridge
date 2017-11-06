f65b6fde604a672693d3a65dc1718fd0933465aa
darren siepka <D@siepka.me.uk>
2017-11-06 10:11:55 +0000

    added new hardware serial example r

--- Serial test programs and samples/sample programs/simple_hardware_get_realtime_example_r/comms.h
+++ Serial test programs and samples/sample programs/simple_hardware_get_realtime_example_r/comms.h
@@ -1,7 +1,7 @@
 #ifndef COMMS_H
 #define COMMS_H
 
-uint16_t Gdata[8];
+uint8_t cmdreply;
 uint8_t realtimebuffer[60];   //buffer array to store the realtime data read
 volatile uint8_t replylength;          // the length of the reply variable
 
